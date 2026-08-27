753407859f05f6704c1d512e1a0fe3c95da711b0
Juan Jose Mata <juanjo.mata@gmail.com>
2026-08-27 12:05:54 -0700

    Remove pre-release Keychain migrations

--- App/KeychainStore.swift
+++ App/KeychainStore.swift
@@ -3,22 +3,17 @@ import Security
 
 enum KeychainStore {
   private static let service = "am.sure.insights"
-  private static let legacyService = "am.sure.native"
 
   @discardableResult
   static func save(_ value: String, account: String, scope: Scope = .deviceOnly) -> Bool {
     guard !value.isEmpty else {
-      delete(account: account, service: service, scope: scope)
-      if scope == .iCloud {
-        delete(account: account, service: service, scope: .deviceOnly)
-        delete(account: account, service: legacyService, scope: .deviceOnly)
-      }
+      delete(account: account, scope: scope)
       return false
     }
 
     let encoded = Data(value.utf8)
     let status = SecItemUpdate(
-      query(account: account, service: service, scope: scope) as CFDictionary,
+      query(account: account, scope: scope) as CFDictionary,
       [kSecValueData as String: encoded] as CFDictionary
     )
     if status == errSecSuccess {
@@ -28,38 +23,14 @@ enum KeychainStore {
       return false
     }
 
-    var attributes = query(account: account, service: service, scope: scope)
+    var attributes = query(account: account, scope: scope)
     attributes[kSecValueData as String] = encoded
     attributes[kSecAttrAccessible as String] = scope.accessibility
     return SecItemAdd(attributes as CFDictionary, nil) == errSecSuccess
   }
 
   static func read(account: String, scope: Scope = .deviceOnly) -> String? {
-    if let value = read(account: account, service: service, scope: scope) {
-      return value
-    }
-
-    let migrationSources: [(String, Scope)] = scope == .iCloud
-      ? [(service, .deviceOnly), (legacyService, .deviceOnly)]
-      : [(legacyService, .deviceOnly)]
-    for (sourceService, sourceScope) in migrationSources {
-      guard let value = read(account: account, service: sourceService, scope: sourceScope) else {
-        continue
-      }
-      if save(value, account: account, scope: scope) {
-        delete(account: account, service: sourceService, scope: sourceScope)
-      }
-      return value
-    }
-    return nil
-  }
-
-  static func contains(account: String, scope: Scope = .deviceOnly) -> Bool {
-    read(account: account, service: service, scope: scope) != nil
-  }
-
-  private static func read(account: String, service: String, scope: Scope) -> String? {
-    var lookup = query(account: account, service: service, scope: scope)
+    var lookup = query(account: account, scope: scope)
     lookup[kSecReturnData as String] = true
     lookup[kSecMatchLimit as String] = kSecMatchLimitOne
     var result: AnyObject?
@@ -68,11 +39,15 @@ enum KeychainStore {
     return String(data: data, encoding: .utf8)
   }
 
-  private static func delete(account: String, service: String, scope: Scope) {
-    SecItemDelete(query(account: account, service: service, scope: scope) as CFDictionary)
+  static func contains(account: String, scope: Scope = .deviceOnly) -> Bool {
+    read(account: account, scope: scope) != nil
   }
 
-  private static func query(account: String, service: String, scope: Scope) -> [String: Any] {
+  private static func delete(account: String, scope: Scope) {
+    SecItemDelete(query(account: account, scope: scope) as CFDictionary)
+  }
+
+  private static func query(account: String, scope: Scope) -> [String: Any] {
     [
       kSecClass as String: kSecClassGenericPassword,
       kSecAttrService as String: service,
